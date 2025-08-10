2451be9056fd381f1eb1fb06ab968001c0cb1612
Kalmax dos Santos Sousa <kalmax.santos.123@gmail.com>
2025-08-09 22:38:38 -0300

    feat: add sign-in

--- src/app/authentication/components/sign-in-form.tsx
+++ src/app/authentication/components/sign-in-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -22,6 +24,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z.object({
   email: z.email("Email inválido"),
@@ -31,6 +34,8 @@ const formSchema = z.object({
 type FormValues = z.infer<typeof formSchema>;
 
 const SignInForm = () => {
+  const router = useRouter();
+
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -39,8 +44,21 @@ const SignInForm = () => {
     },
   });
 
-  function onSubmit(data: FormValues) {
-    console.log("Form submitted:", data);
+  async function onSubmit(values: FormValues) {
+    await authClient.signIn.email({
+      email: values.email,
+      password: values.password,
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: (error) => {
+          toast.error("Usuario ou senha inválidos.");
+          form.setError("email", {});
+          form.setError("password", {});
+        },
+      },
+    });
   }
 
   return (
